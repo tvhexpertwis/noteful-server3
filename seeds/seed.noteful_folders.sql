973bd8527b57f999d105a0b8c782d998ff653414
Kevin Wei <chenghaowei@gmail.com>
2019-10-08 01:07:22 -0400

    add sql migrations and seeds

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,7 @@
+INSERT INTO noteful_folders ( folder_name )
+VALUES 
+  ('recipes'),
+  ('book summaries'),
+  ('presentations'),
+  ('lectures'),
+  ('blueprints');
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,7 @@
+INSERT INTO noteful_notes ( note_name, content, folder_id )
+VALUES 
+  ('Pound Cake', 'lorem ipsum 1', 1),
+  ('Tom Sawyer', 'lorem ipsum 2', 2),
+  ('Alternative Energy', 'lorem ipsum 3', 3),
+  ('SQL and Express', 'lorem ipsum 4', 4),
+  ('Megazord', 'lorem ipsum 5', 5);
